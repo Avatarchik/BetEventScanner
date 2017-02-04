e946e242eed20c4ed8831a1baf75b7318b0a4b0c
ZherditskiyAndrey <josaint13@gmail.com>
2017-02-04 13:47:31 +0200

    add Controllers/TestController.cs

---
+++ BetEventScanner.Web.Client/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Http;
+using BetEventScanner.Common.DataModel;
+using BetEventScanner.Common.Services;
+
+namespace BetEventScanner.Web.Client.Controllers
+{
+    public class TestController : ApiController
+    {
+        private readonly IRepository<LeagueOdds> _oddsRepository;
+
+        public TestController()
+        {
+            _oddsRepository = new OddsRepository<LeagueOdds>();
+        }
+
+        public void Add()
+        {
+            _oddsRepository.InsertEntity(new LeagueOdds
+            {
+                Division = Division.EnglandApl,
+                Source = OddsSource.BetClickCom,
+                OriginSourceLeagueId = 666,
+                Name = "Test",
+                Created = DateTime.UtcNow
+            });
+        }
+    }
+}
